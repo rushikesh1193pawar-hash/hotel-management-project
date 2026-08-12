12482285eacc37f17b1ad8727f183dfcbccd9b9b
rushikesh1193pawar-hash <rushikesh1193pawar@gmail.com>
2026-08-12 13:43:08 +0530

    Update 06_triggers.sql

--- 06_triggers.sql
+++ 06_triggers.sql
@@ -8,9 +8,11 @@ CREATE TRIGGER update_room_status
 AFTER INSERT ON bookings
 FOR EACH ROW
 BEGIN
-    UPDATE rooms
-    SET status = 'Booked'
-    WHERE room_id = NEW.room_id;
+
+UPDATE rooms
+SET status = 'Booked'
+WHERE room_id = NEW.room_id;
+
 END //
 
 DELIMITER ;
